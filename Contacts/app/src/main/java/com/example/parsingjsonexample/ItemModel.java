29d428e817af79f28a75f88e2e7ff9d497cb138b
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-04-12 00:24:03 +0200

    Manage contacts

---
+++ Contacts/app/src/main/java/com/example/parsingjsonexample/ItemModel.java
@@ -0,0 +1,49 @@
+package com.example.parsingjsonexample;
+
+public class ItemModel {
+    String id;
+    String name;
+    String email;
+    String job;
+    String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
