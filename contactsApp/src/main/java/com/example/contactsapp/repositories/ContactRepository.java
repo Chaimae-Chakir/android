08466609ea174cc3980c1ab78ad5b4c2cf0bb9b7
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-04-15 17:51:03 +0200

    delete a folder

--- contactsApp/src/main/java/com/example/contactsapp/repositories/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.contactsapp.repositories;
-
-import com.example.contactsapp.entities.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact,Long> {
-}
