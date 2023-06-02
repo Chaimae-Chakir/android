08163a6062e02dc50ff2e9881d9df5c4c9451ae7
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-06-02 03:40:33 +0100

    fixing some errors

--- SplashScreenWithLogin/app/src/main/java/com/example/splashscreenwithlogin/UserInformationActivity.java
+++ SplashScreenWithLogin/app/src/main/java/com/example/splashscreenwithlogin/UserInformationActivity.java
@@ -24,26 +24,28 @@ public class UserInformationActivity extends AppCompatActivity {
         backButton = findViewById(R.id.back_button);
 
         Intent intent = getIntent();
-        if (intent != null) {
+        if (intent != null && intent.hasExtra("email") && intent.hasExtra("password")) {
             String email = intent.getStringExtra("email");
             int index = email.indexOf("@");
-            String nom = email.substring(0, index);
-            String password = intent.getStringExtra("password");
+            if (index != -1) {
+                String nom = email.substring(0, index);
+                String password = intent.getStringExtra("password");
 
-            String n = "Nom de l'utilisateur est " + nom ;
-            String p =  "Mot de passe est " + password;
+                String n = "Nom de l'utilisateur est " + nom;
+                String p = "Mot de passe est " + password;
 
-            emailTextView.setText(n);
-            passwordTextView.setText(p);
+                emailTextView.setText(n);
+                passwordTextView.setText(p);
+            }
         }
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UserInformationActivity.this, Activity_Splash_Login.class);
-                startActivity(intent);
+
+        backButton.setOnClickListener(v -> {
+                Intent intent1 = new Intent(UserInformationActivity.this, Activity_Splash_Login.class);
+                startActivity(intent1);
                 finish();
-            }
-        });
+            });
+        }
     }
-}
+
+
